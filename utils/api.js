765fa83c843d8d739903c1f700621d82cfaa86c7
fuser <vnovodid@gmail.com>
2020-05-11 16:17:47 +0300

    feat: Basic project set up

---
+++ utils/api.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-unused-vars
+import AsyncStorage from '@react-native-community/async-storage';
+
+
+//return all of the decks along with their titles,
+//questions, and answers.
+export const getDecks = () => {
+
+};
+
+//take in a single id argument and return
+//the deck associated with that id.
+export const getDeck = () => {
+
+};
+
+//take in a single title argument and add it to the decks.
+export const saveDeckTitle = () => {
+
+};
+
+//take in two arguments, title and card, and will add
+//the card to the list of questions for the deck with
+//the associated title.
+export const addCardToDeck = () => {
+
+};
